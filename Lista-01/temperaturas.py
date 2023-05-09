e9f840e91e4dd59b05400333c9b91f349f04684d
lsantanar <contatolsr@hotmail.com>
2023-05-09 20:06:12 -0300

    modificações em condicionais e variáveis

--- Lista-01/temperaturas.py
+++ Lista-01/temperaturas.py
@@ -3,19 +3,33 @@ print("Registro de Temperaturas")
 print("*********************************")
 
 temperaturas = [-3.55, -10.55, 10.12, 11.44, 22.22]
-temperaturas_negativas = [-3.55, -10.55]
-temperaturas_positivas = [10.12, 11.44, 22.22]
+temp_negativas = 0
+temp_positivas = 0
 
 # A menor temperaturas das informadas:
 menor_temperatura = min(temperaturas)
-print("A menor temperatura é: ", menor_temperatura)
+print("        ")
+print("Menor temperatura registrada: ", menor_temperatura)
+print("        ")
 
 # A média das temperaturas que forem > que 0 e < que 20:
 media = sum(temperaturas) / len(temperaturas)
 
+while media > 0:
+    print("Média da temperatura >>>> 0: ", str(media))
+    if media < 20:
+        print("Média da temperatura <<<< 20: ", str(media))
+    break
+print("        ")
+
+# A quantidade de números negativos:
 for item in temperaturas:
-    if media > 0:
-        print("A média da temperatura acima de 0 é : ", str(media))
+    if item < 0:
+        temp_negativas += 1
+print("Números negativos: ", temp_negativas)
 
-
-   
+# A quantidade de números positivoss:
+for item in temperaturas:
+    if item > 0:
+        temp_positivas += 1
+print("Números positivos: ", temp_positivas)
