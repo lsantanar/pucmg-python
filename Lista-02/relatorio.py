399b5491a20378de239fd94d5ddfedba84423b11
lsantanar <contatolsr@hotmail.com>
2023-05-21 17:51:19 -0300

    Exercicio 2: Relatorio

--- Lista-02/relatorio.py
+++ Lista-02/relatorio.py
@@ -10,18 +10,13 @@ while True:
 
     if rsocial == "sair" or total == "0":
         break
-print("")
-print("Cadastro encerrado!")
-# Exibe maior comprador
-# maior_comprador = max(valor_total)
-# print("Lista de compras: ", razao_social[valor_total.index(maior_comprador)])
-
+print("---- Cadastro encerrado! ----")
 print("")
 valor_total.sort(reverse=True)
-print("Total da compra: ",valor_total)
+print("Array de compras:", valor_total)
+print("")
 
-# print("")
-# razao_social.sort(reverse=False)
-# print("Total da compra: ",razao_social.index(valor_total))
+chave_id = list(zip(razao_social, valor_total))
 
-
+for razao_social, valor_total in chave_id:
+    print("Razão social: ", razao_social, "Valor total: ", valor_total)
